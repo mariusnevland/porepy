44410ea8e4e0baedfcbdc52f581b1b713ee283fa
Jhabriel Varela <jhabriel@gmail.com>
2022-06-27 17:07:15 +0200

    WIP: Update time-dependent source terms

--- code_verification/poroelasticity/manufactured_biot/biot_manufactured.py
+++ code_verification/poroelasticity/manufactured_biot/biot_manufactured.py
@@ -1,4 +1,5 @@
 #%% Import modules
+from time import time
 import porepy as pp
 import numpy as np
 import scipy.sparse as sps
@@ -20,7 +21,7 @@ class ManufacturedBiot(pp.ContactMechanicsBiot):
         """Create the grid bucket"""
         self.box = {"xmin": 0.0, "xmax": 1.0, "ymin": 0.0, "ymax": 1.0}
         network_2d = pp.FractureNetwork2d(None, None, self.box)
-        mesh_args = self.params.get("mesh_args", {"mesh_size_frac": 0.05})
+        mesh_args = self.params.get("mesh_args", {"mesh_size_frac": 0.1})
         self.gb: pp.GridBucket = network_2d.mesh(mesh_args)
 
     def _source_scalar(self, g: pp.Grid) -> np.ndarray:
@@ -33,16 +34,27 @@ class ManufacturedBiot(pp.ContactMechanicsBiot):
         time = params.get("time")
         return self.exact_sol.integrated_vector_source(g, time)
 
+    def before_newton_loop(self) -> None:
+        """Here we retrieve the time dependent source terms"""
+        t = self.time
+        for g, d in self.gb:
+            exact_scalar_source = self.exact_sol.integrated_scalar_source(g, t)
+            exact_vector_source = self.exact_sol.integrated_vector_source(g, t)
+            d[pp.PARAMETERS][self.scalar_parameter_key]["source"] = exact_scalar_source
+            d[pp.PARAMETERS][self.mechanics_parameter_key]["source"] = exact_vector_source
+
 
 #%%
+
 ex = ExactSolution()
-params = {"time": 0.1}
+params = {"time": 0, "time_step": 0.1, "end_time": 1.0}
 model = ManufacturedBiot(ex, params)
-model.time_step = 0.1
 model.create_grid()
 gb = model.gb
 
+tic = time()
 pp.run_time_dependent_model(model, params)
+print(f"Simulation finished in {time() - tic}")
 
 g = model.gb.grids_of_dimension(2)[0]
 d = model.gb.node_props(model.gb.grids_of_dimension(2)[0])
@@ -50,16 +62,18 @@ d = model.gb.node_props(model.gb.grids_of_dimension(2)[0])
 
 #%% Plot
 cc = g.cell_centers
-tt = 0.1
+tt = 1
 ff = ex.scalar_source("fun")(cc[0], cc[1], tt)
 fsx = ex.vector_source("fun")[0](cc[0], cc[1], tt)
 fsy = ex.vector_source("fun")[1](cc[0], cc[1], tt)
 int_ff = ex.integrated_scalar_source(g, tt)
 int_fs = ex.integrated_vector_source(g, tt)
+p_final = ex.pressure("fun")(cc[0], cc[1], 1)
 
 #pp.plot_grid(g, ff, plot_2d=True, title="Scalar source")
 #pp.plot_grid(g, fsx, plot_2d=True, title="Vector source (x)")
 #pp.plot_grid(g, fsy, plot_2d=True, title="Vector source (y)")
-pp.plot_grid(g, int_ff, plot_2d=True, title="Integrated scalar source QP")
-pp.plot_grid(g, int_fs[::2], plot_2d=True, title="Integrated vector source (x) QP")
-pp.plot_grid(g, int_fs[1::2], plot_2d=True, title="Integrated vector source (y) QP")
+#pp.plot_grid(g, int_ff, plot_2d=True, title="Integrated scalar source QP")
+#pp.plot_grid(g, int_fs[::2], plot_2d=True, title="Integrated vector source (x) QP")
+#pp.plot_grid(g, int_fs[1::2], plot_2d=True, title="Integrated vector source (y) QP")
+pp.plot_grid(g, d[pp.STATE]["p"], plot_2d=True)
