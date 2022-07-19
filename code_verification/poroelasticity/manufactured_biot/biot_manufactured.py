f1e83cb42719a8e7fc59aab4cbb177538f4c50c0
Jhabriel Varela <jhabriel@gmail.com>
2022-07-19 15:30:31 +0200

    DEV: Manufactured solution working

--- code_verification/poroelasticity/manufactured_biot/biot_manufactured.py
+++ code_verification/poroelasticity/manufactured_biot/biot_manufactured.py
@@ -10,10 +10,15 @@ from biot_manufactured_exact import ExactSolution
 from typing import Dict, Optional
 
 
-#%% Inherit class
+#%% Inherit from ContactMechanicsBiot and override relevant methods
 class ManufacturedBiot(pp.ContactMechanicsBiot):
     """Manufactured Biot class"""
-    def __init__(self, exact_sol, params: Optional[Dict] = None, ) -> None:
+
+    def __init__(
+        self,
+        exact_sol,
+        params: Optional[Dict] = None,
+    ) -> None:
         super().__init__(params)
         self.exact_sol = exact_sol
 
@@ -21,25 +26,29 @@ class ManufacturedBiot(pp.ContactMechanicsBiot):
         """Create the grid bucket"""
         self.box = {"xmin": 0.0, "xmax": 1.0, "ymin": 0.0, "ymax": 1.0}
         network_2d = pp.FractureNetwork2d(None, None, self.box)
-        mesh_args = self.params.get("mesh_args", {"mesh_size_frac": 0.025})
+        mesh_args = self.params.get("mesh_args", {"mesh_size_frac": 0.05})
         self.gb: pp.GridBucket = network_2d.mesh(mesh_args)
 
     def before_newton_loop(self) -> None:
         """Here we retrieve the time dependent source terms"""
-        t = self.time
-        dt = self.time_step
-        next_t = t + dt
         for g, d in self.gb:
-            exact_scalar_source = self.exact_sol.integrated_source_flow(g, next_t)
-            exact_vector_source = self.exact_sol.integrated_source_mechanics(g, next_t)
-            d[pp.PARAMETERS][self.scalar_parameter_key]["source"] = exact_scalar_source
-            d[pp.PARAMETERS][self.mechanics_parameter_key]["source"] = exact_vector_source
+            exact_scalar_source = self.exact_sol.integrated_source_flow(g, self.time)
+            exact_vector_source = self.exact_sol.integrated_source_mechanics(
+                g, self.time
+            )
+            # Note that we have to add the scaling of the time step explicitly
+            # This might change when the Assembler class is discontinued, see issue #675
+            d[pp.PARAMETERS][self.scalar_parameter_key]["source"] = (
+                self.time_step * exact_scalar_source
+            )
+            d[pp.PARAMETERS][self.mechanics_parameter_key][
+                "source"
+            ] = exact_vector_source
 
 
-#%%
-
+#%% Retrieve exact solution
 ex = ExactSolution()
-params = {"time": 0, "time_step": 1.0, "end_time": 1.0, "use_ad": True}
+params = {"time": 0, "time_step": 1, "end_time": 1, "use_ad": True}
 model = ManufacturedBiot(ex, params)
 model.create_grid()
 gb = model.gb
@@ -52,12 +61,22 @@ g = model.gb.grids_of_dimension(2)[0]
 d = model.gb.node_props(model.gb.grids_of_dimension(2)[0])
 
 
+#%% Obtain L2-discrete relative error
+p_approx = d[pp.STATE][model.scalar_variable]
+u_approx = d[pp.STATE][model.displacement_variable]
+p_exact = ex.eval_scalar(g, ex.pressure, model.end_time)
+u_exact = np.asarray(ex.eval_vector(g, ex.displacement, model.end_time)).ravel("F")
+
+error_p = ex.l2_relative_error(g, p_exact, p_approx, is_cc=True, is_scalar=True)
+error_u = ex.l2_relative_error(g, u_exact, u_approx, is_cc=True, is_scalar=False)
+
+
 #%% Plot
 cc = g.cell_centers
-ff = ex.eval_scalar(g, ex.source_flow, 1)
-fs = ex.eval_vector(g, ex.source_mechanics, 1)
-int_ff = ex.integrated_source_flow(g, 1)
-int_fs = ex.integrated_source_mechanics(g, 1)
+ff = ex.eval_scalar(g, ex.source_flow, model.end_time)
+fs = ex.eval_vector(g, ex.source_mechanics, model.end_time)
+int_ff = ex.integrated_source_flow(g, model.end_time)
+int_fs = ex.integrated_source_mechanics(g, model.end_time)
 
 #%% Plot sources
 plot_source = False
@@ -65,15 +84,20 @@ if plot_source:
     pp.plot_grid(g, ff * g.cell_volumes, plot_2d=True, title="Scalar source")
     pp.plot_grid(g, fs[0] * g.cell_volumes, plot_2d=True, title="Vector source (x)")
     pp.plot_grid(g, fs[1] * g.cell_volumes, plot_2d=True, title="Vector source (y)")
-    pp.plot_grid(g, int_ff, plot_2d=True, title="Integrated scalar source QP")
-    pp.plot_grid(g, int_fs[::2], plot_2d=True, title="Integrated vector source (x) QP")
-    pp.plot_grid(g, int_fs[1::2], plot_2d=True, title="Integrated vector source (y) QP")
+    # pp.plot_grid(g, int_ff, plot_2d=True, title="Integrated scalar source QP")
+    # pp.plot_grid(g, int_fs[::2], plot_2d=True, title="Integrated vector source (x) QP")
+    # pp.plot_grid(g, int_fs[1::2], plot_2d=True, title="Integrated vector source (y) QP")
 
 # %% Plot pressure
 plot_pressure = True
 if plot_pressure:
     pp.plot_grid(g, d[pp.STATE]["p"], plot_2d=True, title="Approximated pressure")
-    pp.plot_grid(g, ex.eval_scalar(g, ex.pressure, 1), plot_2d=True, title="Exact pressure")
+    pp.plot_grid(
+        g,
+        ex.eval_scalar(g, ex.pressure, model.end_time),
+        plot_2d=True,
+        title="Exact pressure",
+    )
 
 # %% Plot horizontal displacement
 plot_displacement = True
@@ -82,13 +106,13 @@ if plot_displacement:
         g,
         d[pp.STATE]["u"][::2],
         plot_2d=True,
-        title="Approximated horizontal displacement"
+        title="Approximated horizontal displacement",
     )
     pp.plot_grid(
         g,
-        ex.eval_vector(g, ex.displacement, 1)[0],
+        ex.eval_vector(g, ex.displacement, model.end_time)[0],
         plot_2d=True,
-        title="Exact horizontal displacement"
+        title="Exact horizontal displacement",
     )
 
 # %% Plot vertical displacement
@@ -97,17 +121,11 @@ if plot_displacement:
         g,
         d[pp.STATE]["u"][1::2],
         plot_2d=True,
-        title="Approximated vertical displacement"
+        title="Approximated vertical displacement",
     )
     pp.plot_grid(
         g,
-        ex.eval_vector(g, ex.displacement, 1)[1],
+        ex.eval_vector(g, ex.displacement, model.end_time)[1],
         plot_2d=True,
-        title="Exact vertical displacement"
+        title="Exact vertical displacement",
     )
-
-
-
-
-
-
