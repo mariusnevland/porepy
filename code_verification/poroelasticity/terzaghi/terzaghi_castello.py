1b18cd763b4c4dcdefab6abba3437b1b0c6a47ac
Jhabriel Varela <jhabriel@gmail.com>
2022-11-07 10:36:34 +0100

    Update terzaghi_castello.py

--- code_verification/poroelasticity/terzaghi/terzaghi_castello.py
+++ code_verification/poroelasticity/terzaghi/terzaghi_castello.py
@@ -23,15 +23,15 @@ from time import time
 # Model parameters
 params = {
     "use_ad": True,
-    "height": 1.0,  # [m]
-    "mesh_size": 0.05,  # [m]
-    "applied_load": 6e8,  # [N/m]
+    "height": 2.0,  # [m]
+    "num_cells": 40,  # [number of cells]
+    "vertical_load": 6e8,  # [N * m^-1]
     "mu_lame": 2.475e9,  # [Pa],
     "lambda_lame": 1.65e9,  # [Pa],
     "alpha_biot": 1.0,  # [-]
     "permeability": 9.86e-14,  # [m^2],
-    "viscosity": 1e-3,  # [Pa.s],
-    "fluid_density": 1e3,  # [kg/m^3],
+    "viscosity": 1e-3,  # [Pa * s],
+    "specific_weight": 1014 * 9.80665,  # [Pa * m^-1],
     "upper_limit_summation": 1000,
     "plot_results": True,
 }
@@ -42,11 +42,13 @@ model.prepare_simulation()
 c_f = model.consolidation_coefficient()
 dimless_times = np.array([0, 1E-3, 1E-2, 1E-1, 1])
 times = (dimless_times * model.params["height"] ** 2) / c_f
-dimless_dt = 1E-4
+dimless_dt = 1E-3 / 4
 dt = (dimless_dt * model.params["height"] ** 2) / c_f
 
 new_params = params.copy()
-new_params["time_manager"] = pp.TimeManager(schedule=times, dt_init=dt, constant_dt=True)
+new_params["time_manager"] = pp.TimeManager(
+    schedule=times, dt_init=dt, constant_dt=True
+)
 # Run model
 tic = time()
 print("Simulation started...")
@@ -55,4 +57,9 @@ pp.run_time_dependent_model(model, new_params)
 print(f"Simulation finished in {round(time() - tic)} sec.")
 
 
-
+sd = model.mdg.subdomains()[0]
+data = model.mdg.subdomain_data(sd)
+pp.plot_grid(sd, plot_2d=True)
+pp.plot_grid(
+    sd, data[pp.STATE]["p"], plot_2d=True, title="Terzaghi's consolidation problem"
+)
