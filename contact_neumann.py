5f90ea3dd9964ea36c8ea358cdbc7d83994fd65a
mariusnevland <111966739+mariusnevland@users.noreply.github.com>
2023-01-12 11:28:22 +0100

    Custom fracture network
    Contact mechanics simulation with custom fracture network (two intersecting fractures).

--- contact_neumann.py
+++ contact_neumann.py
@@ -10,11 +10,17 @@ class ContactProblem(pp.ContactMechanics):
         endp = np.array([.2, .5])
         xendp = np.array([.2, .8])
         yendp= np.array([.2, 1])
-        self.mdg, self.box = pp.md_grids_2d.single_vertical(mesh_args, endp,simplex=True)
-        # self.mdg, self.box = pp.md_grids_2d.two_intersecting(mesh_args, xendp,yendp,
+        #self.mdg, self.box = pp.md_grids_2d.single_vertical(mesh_args, endp,
         # simplex=True)
-        # self.mdg, self.box = pp.md_grids_2d.seven_fractures_one_L_intersection(
-        # mesh_args)
+
+        # Attempt at making an arbitrary fracture network from scratch.
+        points = np.array([[0.4,0.5],[1.6,0.5],[0.4,0.3],[1.6,0.7]]).T
+        edges = np.array([[0,1],[2,3]]).T
+        self.box = {"xmin": 0, "ymin": 0, "xmax": 2, "ymax": 1}
+        # self.mdg=pp.md_grids_2d.utils.make_mdg_2d_simplex(mesh_args,points,edges,
+        # domain=self.box)
+        network = pp.FractureNetwork2d(points,edges,domain=self.box)
+        self.mdg = network.mesh(mesh_args)
         pp.contact_conditions.set_projections(self.mdg)
 
     # Attempt mixed Dirichlet and Neumann conditions.
@@ -37,8 +43,8 @@ class ContactProblem(pp.ContactMechanics):
         _, east, west, north, south, _, _ = self._domain_boundary_sides(sd)
         values = np.zeros((self.nd, sd.num_faces))
         # Tractions on north and east faces.
-        values[0, east] = 1
-        values[1, north] = -1
+        values[0, east] = -0.1
+        values[1, north] = -0.1
         # Reshape according to PorePy convention
         values = values.ravel("F")
         return values
@@ -52,6 +58,7 @@ params = {"use_ad": True}
 model = ContactProblem(params)
 pp.run_stationary_model(model, params)
 pp.plot_grid(model.mdg, vector_value=model.displacement_variable)
+# pp.plot_grid(model.mdg)
 inds = model.dof_manager.dof_var(var=[model.displacement_variable])
 vals = model.dof_manager.assemble_variable(variables=[model.displacement_variable])
 displacement = vals[inds]
